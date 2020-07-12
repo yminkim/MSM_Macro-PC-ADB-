fcf4947c6f172ceb0ea1a968c1ac09a9c790021c
KIM YOUNGMIN <dudals6116@naver.com>
2020-07-12 22:05:07 +0900

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,28 @@
+class FourCal:
+    def setData(self, first, second):
+        self.first = first
+        self.second = second
+
+    def add(self):
+        result = self.first + self.second
+        return result
+    
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+    
+    def sub(self):
+        result = self.first - self.second
+        return result
+        
+a = FourCal()
+a.setData(8,4)
+print(a.add())
+print(a.mul())
+print(a.div())
+print(a.sub())
+
+#객체를 사용한 계산기...'-` 

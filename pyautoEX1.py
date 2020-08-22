fe0ae55b209c751ecd2b69f98ccabd75cc794408
KIM YOUNGMIN <dudals6116@naver.com>
2020-08-23 01:35:57 +0900

    Create pyautoEX1.py

---
+++ pyautoEX1.py
@@ -0,0 +1,10 @@
+#pip install pyautogui
+import pyautogui
+pyautogui.position()
+pyautogui.moveTo(1133, 479, 0.1)
+pyautogui.click(button='left')
+pyautogui.write('안녕')
+#pyautogui.dragTo(326, 23, 0.2, button='left')
+
+#for i in range(10):
+#    pyautogui.scroll(100)
